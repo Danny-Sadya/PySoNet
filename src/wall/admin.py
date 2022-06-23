85c26c739bec9aeed45260dc3172b6d45a7a7dfb
danny_sadya <klaar081@gmail.com>
2022-06-23 11:29:24 +0500

    Added wall app: post and comment by users. Base permissions, classes and serializers

---
+++ src/wall/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+
+from src.wall.models import Post, Comment
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Posts"""
+    list_display = ("user", "moderation", "create_date", "view_count", "published", "id")
+
+
+@admin.register(Comment)
+class CommentAdmin(MPTTModelAdmin, admin.ModelAdmin):
+    """Comments to a posts"""
+    list_display = ("user", "post", "created_date", "update_date", "published", "id")
+    # actions = ["unpublish", "publish"]
+    mptt_level_indent = 15
