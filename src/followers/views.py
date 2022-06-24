6213fc8b7d7ac4db89a98012a0b046f5b77a0004
danny_sadya <klaar081@gmail.com>
2022-06-24 10:50:59 +0500

    Added unfollowing feature

--- src/followers/views.py
+++ src/followers/views.py
@@ -26,3 +26,21 @@ class AddFollowerView(views.APIView):
             Follower.objects.create(user=user, subscriber=request.user)
             return response.Response(status=201)
         return response.Response(status=404)
+
+    def delete(self, request, pk):
+
+        try:
+            sub = Follower.objects.get(subscriber=request.user, user_id=pk)
+        except Follower.DoesNotExist:
+            return response.Response(status=404)
+
+        sub.delete()
+        return response.Response(status=204)
+
+#
+# class DeleteFollowerView(views.APIView):
+#     """Delete from following"""
+#
+#     permission_classes = [permissions.IsAuthenticated]
+#
+#
